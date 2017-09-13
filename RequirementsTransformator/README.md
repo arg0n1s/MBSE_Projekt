61dd331bf0191b76106d0d061f8eb1a7127d1d3e
Arg0n1s <basti_seppl@yahoo.com>
2017-09-13 21:37:00 +0200

    RequirementsTransformator inserted

---
+++ RequirementsTransformator/README.md
@@ -0,0 +1,1 @@
+RequirementsTransformator
---
+++ RequirementsTransformator/src/RequirementsTransformator/org/moflon/tie/RequirementsTransformatorModelGen.java
@@ -0,0 +1,33 @@
+package RequirementsTransformator.org.moflon.tie;
+
+import java.io.IOException;
+import org.apache.log4j.BasicConfigurator;
+import org.moflon.tgg.algorithm.synchronization.SynchronizationHelper;
+
+
+import org.moflon.tgg.algorithm.modelgenerator.ModelGenerator;
+import org.moflon.tgg.algorithm.modelgenerator.controller.*;
+
+import RequirementsTransformator.RequirementsTransformatorPackage;
+
+
+public class RequirementsTransformatorModelGen extends SynchronizationHelper{
+
+   public RequirementsTransformatorModelGen()
+   {
+      super(RequirementsTransformatorPackage.eINSTANCE, ".");
+   }
+	
+	public static void main(String[] args) throws IOException {
+		// Set up logging
+		BasicConfigurator.configure();
+
+		AbstractModelGenerationController controller = new DefaultModelGenController();
+		controller.addContinuationController(new MaxRulePerformCounterController(20));
+      	controller.addContinuationController(new TimeoutController(5000));
+      	controller.setRuleSelector(new LimitedRandomRuleSelector().addRuleLimit("<enter rule name>", 1));
+
+		ModelGenerator gen = new ModelGenerator(RequirementsTransformatorPackage.eINSTANCE, controller);
+		gen.generate();
+	}
+}
